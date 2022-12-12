51ae6557b2b1f31c8a57765b596e58994caaef5a
krish <krishp2@illinois.edu>
2022-12-12 09:46:58 +0000

    h

--- BFS.h
+++ BFS.h
@@ -1,10 +1,2 @@
 #pragma once
 #include <iostream>
-#include <string>
-
-#include "Graph.h"
-using std::string;
-
-void BFSUtil(int v, vector<bool> visited, Graph obj1);
-bool isSC(Graph obj, const string& d);
-void distance(int v, Graph obj1);
